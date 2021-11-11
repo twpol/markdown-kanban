779f8180c8be1b78fbbbf7f9078b131a7fd30245
James Ross <git@james-ross.co.uk>
2021-11-11 20:19:28 +0000

    feat: Initial .NET application

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Markdown_Kanban
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
